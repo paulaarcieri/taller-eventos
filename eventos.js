3c655b2fa73bf8190dda110d44ccfb0e813b2908
unknown <pauarcieri@gmail.com>
2023-08-11 15:10:42 -0300

    addEventListener con 'click' agregado

---
+++ eventos.js
@@ -0,0 +1,9 @@
+
+function button() {
+    return alert("Hola! Soy el div");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let div = document.getElementById('boton');
+    div.addEventListener("click", button);
+});
